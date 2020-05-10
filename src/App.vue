c857f35697c882aadd5b46d0fdf765fab0383d3d
Karen Matala <karenirene.cano1203@gmail.com>
2020-05-10 11:24:28 +0800

    Create signed in state to show sign in and sign out components accordingly

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <template>
-  <div id="app">
-    <amplify-authenticator></amplify-authenticator>
-    <amplify-sign-out></amplify-sign-out>
-  </div>
+    <div id="app">
+      <div v-if="!signedIn">
+         <amplify-authenticator></amplify-authenticator>
+      </div>
+      <div v-if="signedIn">
+        <amplify-sign-out class="signout"></amplify-sign-out>
+        <h1>Hello World</h1>
+      </div>
+    </div>
 </template>
 <script>
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import { Auth } from 'aws-amplify'
 export default {
   name: 'app',
-  components: {
+  async beforeCreate() {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    });
+  },
+  data () {
+    return {
+      signedIn: false    }
   }
 }
 </script>
+
 <style>
+body {
+  margin: 0
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.signout {
+  background-color: #ededed;
+  margin: 0;
+  padding: 11px 0px 1px;
 }
 </style>
